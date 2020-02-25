6867c680da677fe03caaebc0b6393cf6e79fa6de
Yujun Chen <33580541+ychen246@users.noreply.github.com>
2020-02-25 17:10:55 -0600

    js behavior fixed
    now properly random pick dog pic

--- js/proj1.js
+++ js/proj1.js
@@ -13,19 +13,20 @@ img.addEventListener('click', randomPic);
 
 function randomPic(event) {
     var random =Math.ceil(Math.random() * 4);
+    //console.log(random);
 
     switch(random){
         case 1:
-            document.getElementById('dogpic').src = "dog1.jpg";
+            document.getElementById('dogpic').src = "img/dog1.jpg";
             break;
         case 2:
-            document.getElementById('dogpic').src = "dog2.jpeg";
+            document.getElementById('dogpic').src = "img/dog2.jpeg";
             break;
         case 3:
-            document.getElementById('dogpic').src = "dog3.jpg";
+            document.getElementById('dogpic').src = "img/dog3.jpg";
             break;
         case 4:
-            document.getElementById('dogpic').src = "dog4.jpg";
+            document.getElementById('dogpic').src = "img/dog4.jpg";
             break;
     }
   }
