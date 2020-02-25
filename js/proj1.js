76e6743db942479b8e6dcb732306ff104a4d3d14
Yujun Chen <33580541+ychen246@users.noreply.github.com>
2020-02-25 17:04:48 -0600

    js needs fixing

--- js/proj1.js
+++ js/proj1.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+var form = document.getElementById('form');
 
 form.addEventListener('submit', logSubmit);
 
@@ -7,6 +7,25 @@ function logSubmit(event) {
     window.alert("There is no backend to handle information");
   }
   
+var img = document.getElementById('image_button')
 
+img.addEventListener('click', randomPic);
 
-  
+function randomPic(event) {
+    var random =Math.ceil(Math.random() * 4);
+
+    switch(random){
+        case 1:
+            document.getElementById('dogpic').src = "dog1.jpg";
+            break;
+        case 2:
+            document.getElementById('dogpic').src = "dog2.jpeg";
+            break;
+        case 3:
+            document.getElementById('dogpic').src = "dog3.jpg";
+            break;
+        case 4:
+            document.getElementById('dogpic').src = "dog4.jpg";
+            break;
+    }
+  }
